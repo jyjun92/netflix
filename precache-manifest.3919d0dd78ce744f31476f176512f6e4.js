6544e9dbab3399341f1a4b45bbbaa3d490bedd9c
Jea Yoen Jun <jea.jun422@gmail.com>
2020-03-29 17:40:42 -0500

    Updates

---
+++ precache-manifest.3919d0dd78ce744f31476f176512f6e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac7332e15621c6771d7df00464bf1d3b",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "8e08d161ff1e2aacde9d",
+    "url": "/netflix/static/css/main.9ef02fc0.chunk.css"
+  },
+  {
+    "revision": "965d0e5a3abcb333ee77",
+    "url": "/netflix/static/js/2.210750b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/netflix/static/js/2.210750b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e08d161ff1e2aacde9d",
+    "url": "/netflix/static/js/main.ef9c20cd.chunk.js"
+  },
+  {
+    "revision": "1b14c35bdbc2836c3b64",
+    "url": "/netflix/static/js/runtime-main.38b426f0.js"
+  }
+]);
